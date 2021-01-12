7d8924edbb321fd7f9c47e823654e713598eca5f
Uday Bhaskar Gajavalli <udaygajavalli@gmail.com>
2021-01-12 20:14:25 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'form-dashboard';
+  users = [];
+
+  getUserData(ngFormObj: NgForm) {
+    let userObj = ngFormObj.value;
+    //push userObj to users array
+    this.users.push(userObj);
+
+    ngFormObj.reset();
+  }
+
+  deleteUserObj(index) {
+    this.users.splice(index, 1);
+  }
 }
